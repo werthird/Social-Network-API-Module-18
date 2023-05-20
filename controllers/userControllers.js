d777c92d6b99ea658306a835f1a82db269f129bf
Devin Reilly <werthird2016@gmail.com>
2023-05-20 19:59:25 -0400

    added routes to add and delete friends from user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -103,4 +103,51 @@ module.exports = {
     };
   },
 
+
+  //===========================================================
+  // ADD FRIEND TO USER
+  async addFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId},
+        { $addToSet: { friends: req.body.friendId } },
+        { new: true }
+      );
+
+      if (!user) {
+        res.status(404).json({ message: 'No user with that ID' });
+      };
+
+      res.status(200).json({ user, message: 'Friend added' });
+
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).json(err);
+    };
+  },
+
+
+  //===========================================================
+  // DELETE FRIEND FROM USER
+  async deleteFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId},
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      );
+
+      if (!user) {
+        res.status(404).json({ message: 'No user with that ID' });
+      };
+
+      res.status(200).json({ user, message: 'Friend deleted' });
+
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).json(err);
+    };
+  },
+
+
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,13 +4,19 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUserEmail,
   deleteUser,
-  updateUserEmail
+  addFriend,
+  deleteFriend
 } = require('../../controllers/userControllers');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).put(updateUserEmail).delete(deleteUser);
+
+router.route('/:userId/friends').post(addFriend);
+
+router.route('/:userId/friends/:friendId').delete(deleteFriend);
 
 
 module.exports = router;
